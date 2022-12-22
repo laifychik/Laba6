83e92e413276b67f5cb1c3c9fbd01586c3a333c1
laifychik <kuznevp@gmail.com>
2022-12-22 10:37:52 +0300

    Changes

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-class Calculator:
-  def __init__(self):
-    pass
-  def add(self, x1, x2):
-    return x1 + x2
-  def multiply(self, x1, x2):
-    return x1 * x2
-  def subtract(self, x1, x2):
-    return x1 - x2
-  def divide(self, x1, x2):
-    if x2 != 0:
-      return x1/x2
+import unittest
+
+from Calculator import Calculator
+class TestCalculator(unittest.TestCase):
+  def setUp(self):
+    self.calculator = Calculator()
+  def test_add(self):
+    self.assertEqual(self.calculator.add(4,7), 11)
+  def test_subtract(self):
+    self.assertEqual(self.calculator.subtract(10,5), 5)
+  def test_multiply(self):
+    self.assertEqual(self.calculator.multiply(3,7), 21)
+  def test_divide(self):
+    self.assertEqual(self.calculator.divide(10,2), 5)
+if __name__ == "__main__":
+  unittest.main()
